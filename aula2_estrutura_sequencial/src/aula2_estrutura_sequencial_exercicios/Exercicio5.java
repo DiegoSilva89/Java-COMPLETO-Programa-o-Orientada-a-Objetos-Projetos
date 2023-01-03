752c493ff1bbbb6052f38095f641551a12f77fb9
Diego Silva <diegosantosdasilva89@gmail.com>
2023-01-03 01:21:28 -0300

    Acrescentado enunciado dos exercios 5 e 6 da aula 2

--- aula2_estrutura_sequencial/src/aula2_estrutura_sequencial_exercicios/Exercicio5.java
+++ aula2_estrutura_sequencial/src/aula2_estrutura_sequencial_exercicios/Exercicio5.java
@@ -6,6 +6,9 @@ import java.util.Scanner;
 public class Exercicio5 {
 
 	public static void main(String[] args) {
+		
+		/*Fazer um programa para ler o código de uma peça 1, o número de peças 1, o valor unitário de cada peça 1, o 
+		código de uma peça 2, o número de peças 2 e o valor unitário de cada peça 2. Calcule e mostre o valor a ser pago.*/
 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
