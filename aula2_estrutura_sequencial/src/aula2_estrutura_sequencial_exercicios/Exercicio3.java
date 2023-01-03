93148b26de9794361f35f92c206c4404584ebcc9
Diego Silva <diegosantosdasilva89@gmail.com>
2023-01-03 01:17:26 -0300

    Acrescentado enunciado do exercio 3 da aula 2

--- aula2_estrutura_sequencial/src/aula2_estrutura_sequencial_exercicios/Exercicio3.java
+++ aula2_estrutura_sequencial/src/aula2_estrutura_sequencial_exercicios/Exercicio3.java
@@ -6,6 +6,10 @@ public class Exercicio3 {
 
 	public static void main(String[] args) {
 		
+		
+		/*Fazer um programa para ler quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto 
+		de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D) */
+		
 		Scanner sc = new Scanner(System.in);
 		
 		int A = sc.nextInt();
